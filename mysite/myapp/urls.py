237e7739e1b1a42b1cd2fed86ed8beabb98fdf46
mohibiology <abdulmohib1408@gmail.com>
2025-09-11 16:05:52 +0500

    Templates

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+
+app_name = 'myapp'
+
 urlpatterns = [
-    path('', views.index),
-    path('item/', views.item)
+    path('', views.index, name='index'),
+    path('item/', views.item),
+    path('<int:id>',views.detail, name='detail')
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -4,7 +4,17 @@ from .models import Item
 # Create your views here.
 def index(request):
     item_list = Item.objects.all()
-    return HttpResponse(item_list)
+    context = {
+        "item_list": item_list
+    }
+    return render(request, 'myapp/index.html',context)
 
 def item(request):
     return HttpResponse("Item")
+
+def detail(request, id):
+    item = Item.objects.get(id=id)
+    context = {
+        "item":item
+    }
+    return render(request, 'myapp/detail.html', context)
