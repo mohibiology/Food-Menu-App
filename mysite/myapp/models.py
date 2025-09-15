353301c959d95ce44b9e354719e7a6e9d551ed0d
mohibiology <abdulmohib1408@gmail.com>
2025-09-15 20:05:52 +0500

    Tailwind Integration

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -7,5 +7,6 @@ class Item(models.Model):
         return self.item_name
     
     item_name = models.CharField(max_length=100)
-    item_description = models.CharField()
+    item_description = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500, default='https://thumbs.dreamstime.com/b/laughing-cooking-couple-kitchen-silly-meal-prep-morning-joke-smile-apartment-relationship-man-affection-339924493.jpg')
