43b0dbf4f49fcfc3b41e806e0ff122804c16132b
mohibiology <abdulmohib1408@gmail.com>
2025-10-03 19:13:19 +0500

    Databases completed

--- mysite/myapp/managers.py
+++ mysite/myapp/managers.py
@@ -1,8 +1,7 @@
 from django.db import models
 class ItemManager(models.Manager):
-    def cheap_items(self):
-        return self.filter(item_price__lt=2)
-    def expensive_items(self):
-        return self.filter(item_price__gt=2)
-    def search(self, keyword):
-        return self.filter(item_name__icontains=keyword)
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted=False)
+    
+    def deleted(self):
+        return super().get_queryset().filter(is_deleted=True)
--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
@@ -18,6 +19,11 @@ class Item(models.Model):
     def success_url(self):
         return reverse('myapp:index')
     
+    def delete(self, using = None, keep_parents = False):
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save()
+    
     user_name = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     item_name = models.CharField(max_length=100, db_index=True)
     item_description = models.CharField(max_length=200)
@@ -25,6 +31,8 @@ class Item(models.Model):
     item_image = models.URLField(max_length=500, default='https://thumbs.dreamstime.com/b/laughing-cooking-couple-kitchen-silly-meal-prep-morning-joke-smile-apartment-relationship-man-affection-339924493.jpg')
     is_available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(null=True,blank=True)
 
     objects = ItemManager()
 
